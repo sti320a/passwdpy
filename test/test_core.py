691571e4e5c604e9dfeaccde69d44c64c589c04c
Tomoaki Sugiayma <sti320a@gmail.com>
2019-08-24 21:36:44 +0900

    Update testcase

--- test/test_core.py
+++ test/test_core.py
@@ -7,3 +7,19 @@ class TestCore(TestCase):
     def test_generate_passwd_length(self):
         len_passwd = len(generate_passwd(10))
         self.assertEqual(len_passwd, 10)
+
+    def test_generate_passwd_complexity(self):
+        import string
+        target_passwd = generate_passwd(1)
+        lower_cases = string.ascii_lowercase
+        is_included_lowercase = True if set(
+            list(target_passwd)
+        ) & set(
+            list(lower_cases)
+        ) else False
+        self.assertEqual(is_included_lowercase, True)
+
+
+if __name__ == '__main__':
+    test = TestCore()
+    test.test_generate_passwd_complexity()
