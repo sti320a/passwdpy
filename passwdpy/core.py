028bb0358ccc132fa235cb0392434921463627f2
Tomoaki Sugiayma <sti320a@gmail.com>
2019-08-24 20:33:18 +0900

    Refactor generate_ramdom_str

--- passwdpy/core.py
+++ passwdpy/core.py
@@ -3,6 +3,10 @@ import string
 
 
 def generate_random_str(length):
-    randomlst = [random.choice(string.ascii_letters+string.digits)
-                 for i in range(length)]
+    randomlst = random.choices(string.ascii_letters+string.digits, k=length)
     return ''.join(randomlst)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print(generate_random_str(n))
